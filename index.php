b8288c44c89c2cb927d5f289f8118af10a682834
fernando henrique <fernandomk6@gmail.com>
2022-05-06 22:20:22 -0300

    configurações iniciais

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Kylogramer</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://fonts.sandbox.google.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <h1>Kylogramer</h1>
+  <span class="material-symbols-outlined">
+    search
+  </span>
+  <span class="material-symbols-outlined">
+    settings
+  </span>
+</body>
+</html>
