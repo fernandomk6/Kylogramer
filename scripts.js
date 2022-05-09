c89768c84429fe5faf22ab00e1ef3da9b932920f
fernando henrique <fernandomk6@gmail.com>
2022-05-09 17:45:13 -0300

    concluido evento nav bar

--- scripts.js
+++ scripts.js
@@ -2,17 +2,14 @@ function showNav() {
   
   let btnShowNav = document.querySelector("#btn-show-nav");
   let nav = document.querySelector("#nav");
-  let navActions = document.querySelector("#nav-actions");
   let btnCloseNav = document.querySelector("#btn-close-nav");
 
   btnShowNav.addEventListener("click", function() {
     nav.classList.add("--nav-show");
-    navActions.classList.add("--show");
   });
 
   btnCloseNav.addEventListener("click", function() {
     nav.classList.remove("--nav-show");
-    navActions.classList.remove("--show");
   });
 
 }
