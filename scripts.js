7027eadd15aad1abc1aad00e6ea5fd4c16804485
fernando henrique <fernandomk6@gmail.com>
2022-05-09 16:50:12 -0300

    resolver problema de importants e concluir eventos nav

--- scripts.js
+++ scripts.js
@@ -2,10 +2,17 @@ function showNav() {
   
   let btnShowNav = document.querySelector("#btn-show-nav");
   let nav = document.querySelector("#nav");
+  let navActions = document.querySelector("#nav-actions");
+  let btnCloseNav = document.querySelector("#btn-close-nav");
 
   btnShowNav.addEventListener("click", function() {
-    nav.classList.toggle("--show");
-    nav.classList.toggle("nav--mobile");
+    nav.classList.add("--nav-show");
+    navActions.classList.add("--show");
+  });
+
+  btnCloseNav.addEventListener("click", function() {
+    nav.classList.remove("--nav-show");
+    navActions.classList.remove("--show");
   });
 
 }
