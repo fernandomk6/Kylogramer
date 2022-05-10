6d85fc911c9af077a13b518090734d540a4e7a49
fernando henrique <fernandomk6@gmail.com>
2022-05-10 14:02:59 -0300

    refatorado css, excluido classes desnecessarias e colocado em pretica class por função e não por elemento

--- clientes.php
+++ clientes.php
@@ -4,59 +4,63 @@
 <body>
   <?php require("./templates/header.html"); ?>
   <main>
-    <div id="actions" class="actions">
-      <div class="actions-container">
-        <button id="btn-show-nav" class="btn btn--icon btn--hamburguer">
-          <span class="material-symbols-outlined">
-            menu
-          </span>
-        </button>
-        <button id="btn-add" class="btn btn--icon">
-          <span class="material-symbols-outlined">
-            add
-          </span>
-        </button>
-      </div>
-    </div>
-    <div id="searc" class="search">
-      <div class="search-container">
-        <form>
-          <input type="hidden" name="table" value="cliente">
-          <input type="hidden" name="operation" value="search">
-          <input type="text" name="nome" placeholder="Nome do cliente">
-          <button type="submit" class="btn btn--icon">
+    <section id="actions" class="section">
+      <div class="section-container">
+        <div class="row-between">
+          <button id="btn-show-nav" class="btn btn--icon btn--hamburguer">
             <span class="material-symbols-outlined">
-              search
+              menu
             </span>
           </button>
+          <button id="btn-add" class="btn btn--icon">
+            <span class="material-symbols-outlined">
+              add
+            </span>
+          </button>
+        </div>
+      </div>
+    </section>
+    <section id="search" class="section">
+      <div class="section-container">
+        <form class="form-row">
+          <div class="input-container">
+            <input class="input-default" type="text" name="nome" placeholder="Nome do cliente">
+          </div>
+          <div class="submit-container">
+            <button type="submit" class="btn btn--icon">
+              <span class="material-symbols-outlined">
+                search
+              </span>
+            </button>
+          </div>
         </form>
       </div>
-    </div>
-    <div id="content" class="content">
-      <div class="content-container">
-        <div class="content-table">
-          <div class="content-thead">
-            <div class="content-tr">
-              <div class="content-th">
+    </section>
+    <section id="content" class="section">
+      <div class="section-container">
+        <div class="table">
+          <div class="thead">
+            <div class="tr">
+              <div class="th">
                 <span>Nome</span>
               </div>
-              <div class="content-th">
+              <div class="th">
                 <span>Telefone</span>
               </div>
-              <div class="content-th content-th--actions">
+              <div class="th th--actions">
                 <span>Ações</span>
               </div>
             </div>
           </div>
-          <div class="content-tbody">
-            <div class="content-tr">
-              <div class="content-td">
+          <div class="tbody">
+            <div class="tr">
+              <div class="td">
                 <span>Fernando</span>
               </div>
-              <div class="content-td">
+              <div class="td">
                 <span>85985680757</span>
               </div>
-              <div class="content-td content-td--actions">
+              <div class="td td--actions">
                 <button class="btn btn--icon">
                   <span class="material-symbols-outlined">
                     delete
@@ -69,14 +73,14 @@
                 </button>
               </div>
             </div>
-            <div class="content-tr">
-              <div class="content-td">
+            <div class="tr">
+              <div class="td">
                 <span>Fernando Henrique Pontes</span>
               </div>
-              <div class="content-td">
+              <div class="td">
                 <span>85985680757</span>
               </div>
-              <div class="content-td content-td--actions">
+              <div class="td td--actions">
                 <button class="btn btn--icon">
                   <span class="material-symbols-outlined">
                     delete
@@ -89,14 +93,14 @@
                 </button>
               </div>
             </div>
-            <div class="content-tr">
-              <div class="content-td">
+            <div class="tr">
+              <div class="td">
                 <span>Fernando Henrique</span>
               </div>
-              <div class="content-td">
+              <div class="td">
                 <span>85985680757</span>
               </div>
-              <div class="content-td content-td--actions">
+              <div class="td td--actions">
                 <button class="btn btn--icon">
                   <span class="material-symbols-outlined">
                     delete
@@ -112,10 +116,39 @@
           </div>
         </div>
       </div>
-    </div>
-    <div id="modal-add">
-      <h2>Modal de cadastro</h2>
-    </div>
+    </section>
+    <section id="add" class="section add">
+      <section class="section-container">
+        <div class="add-actions">
+          <h2>Kylogramer</h2>
+          <button id="btn-close-add" class="btn btn--icon btn--icon--alt">
+            <span class="material-symbols-outlined">
+              close
+            </span>
+          </button>
+        </div>
+        <p class="light-text">Cadastre aqui seus clientes</p>
+        <form class="add-form">
+          <div class="input-container">
+            <label>
+              <span>Nome</span>
+              <input type="text" name="nome" placeholder="Nome do seu cliente">
+            </label>
+            <label>
+              <span>Telefone</span>
+              <input type="number" name="telefone" placeholder="Contato de seu cliente">
+            </label>
+          </div>
+          <div class="submit-container">
+            <button type="submit">
+              <span class="material-symbols-outlined">
+                done
+              </span>
+            </button>
+          </div>
+        </form>
+      </section>
+    </section>
   </main>
   <script src="./scripts.js"></script>
   <script src="./clientes.js"></script>
