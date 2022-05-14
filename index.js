17ea485c8b2edda8c2e05c26d32741a63fdebc3d
fernando henrique <fernandomk6@gmail.com>
2022-05-14 01:36:22 -0300

    concluido layout component header

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 // import Client from "./components/pages/client/Client.js";
-
 // const client = new Client();
-
 // console.log(client);
--- index.php
+++ index.php
@@ -1,7 +1,18 @@
-<?php require_once("./components/Head.html") ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Kylogramer</title>
+  <link rel="stylesheet" href="./index.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+</head>
 <body>
-  <h1>Kylogramer</h1>
-  <?php require_once("./components/pages/client/Client.html") ?>
+  <div class="App">
+    <?php require_once("./components/header/Header.html") ?>
+    <?php require_once("./components/pages/client/Client.html") ?>
+  </div>
   <script type="module" src="./index.js"></script>
 </body>
 </html>
