5a473baa88e4fe5ede10dd7c90e0fc0b4e033e10
fernando henrique <fernandomk6@gmail.com>
2022-05-20 23:20:30 -0300

    refatorado eventos header

--- components/header/Header.js
+++ components/header/Header.js
@@ -1,95 +1,70 @@
 class Header {
 
   constructor() {
-    this.HeaderNavItemClient("Header__nav__item__client");
-    this.HeaderNavItemProduct("Header__nav__item__product");
-    this.HeaderNavItemSale("Header__nav__item__sale");
-    this.HeaderNavItemPayment("Header__nav__item__payment");
+    this.showClientPage();
+    this.showProductPage();
+    this.showSalePage();
+    this.showPaymentPage();
   };
 
-  HeaderNavItemClient(id) {
-    let el = document.getElementById(id);
+  pages = {
+    client: document.getElementById("client"),
+    product: document.getElementById("product"),
+    sale: document.getElementById("sale"),
+    payment: document.getElementById("payment")
+  };
 
-    let pages = [
-      document.getElementById("client"),
-      document.getElementById("product"),
-      document.getElementById("sale"),
-      document.getElementById("payment")
-    ];
-
-    el.onclick = function() {
-      
-      for (const page of pages) {
-        page.classList.add("--hide");
+  hideAllPages() {
+    for (const page in this.pages) {
+      if (Object.hasOwnProperty.call(this.pages, page)) {
+        const element = this.pages[page];
+        element.classList.add("--hide");
       }
+    }
+  }
 
-      document.getElementById("client").classList.remove("--hide");
+  showClientPage() {
 
+    let headerNavItemClient = document.getElementById("Header__nav__item__client");
+
+    headerNavItemClient.onclick = () => {
+      this.hideAllPages();
+      this.pages.client.classList.remove("--hide");
+    };
+
+  };
+
+  showProductPage() {
+    
+    let headerNavItemProduct = document.getElementById("Header__nav__item__product");
+
+    headerNavItemProduct.onclick = () => {
+      this.hideAllPages();
+      this.pages.product.classList.remove("--hide");
     }
 
   };
 
-  HeaderNavItemProduct(id) {
-    let el = document.getElementById(id);
+  showSalePage() {
+    
+    let headerNavItemSale = document.getElementById("Header__nav__item__sale");
 
-    let pages = [
-      document.getElementById("client"),
-      document.getElementById("product"),
-      document.getElementById("sale"),
-      document.getElementById("payment")
-    ];
-
-    el.onclick = function() {
-      
-      for (const page of pages) {
-        page.classList.add("--hide");
-      }
-
-      document.getElementById("product").classList.remove("--hide");
-
+    headerNavItemSale.onclick = () => {
+      this.hideAllPages();
+      this.pages.sale.classList.remove("--hide");
     }
+
   };
 
-  HeaderNavItemSale(id) {
-    let el = document.getElementById(id);
+  showPaymentPage() {
+    
+    let headerNavItemPayment = document.getElementById("Header__nav__item__payment");
 
-    let pages = [
-      document.getElementById("client"),
-      document.getElementById("product"),
-      document.getElementById("sale"),
-      document.getElementById("payment")
-    ];
-
-    el.onclick = function() {
-      
-      for (const page of pages) {
-        page.classList.add("--hide");
-      }
-
-      document.getElementById("sale").classList.remove("--hide");
-
+    headerNavItemPayment.onclick = () => {
+      this.hideAllPages();
+      this.pages.payment.classList.remove("--hide");
     }
-  };
 
-  HeaderNavItemPayment(id) {
-    let el = document.getElementById(id);
-
-    let pages = [
-      document.getElementById("client"),
-      document.getElementById("product"),
-      document.getElementById("sale"),
-      document.getElementById("payment")
-    ];
-
-    el.onclick = function() {
-      
-      for (const page of pages) {
-        page.classList.add("--hide");
-      }
-
-      document.getElementById("payment").classList.remove("--hide");
-
-    }
   };
 
 }
