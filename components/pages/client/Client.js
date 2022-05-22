f5e2064db209da4948f6e96b2e226b8717e3ad1e
fernando henrique <fernandomk6@gmail.com>
2022-05-22 14:59:24 -0300

    openform, cancel form, e submit form concluidos

--- components/pages/client/Client.js
+++ components/pages/client/Client.js
@@ -1,32 +1,139 @@
 class Client {
 
   constructor() {
+    this.form();
+  };
+
+  sections = {
+    pageTitle: document.getElementById("client__page-title__section"),
+    search: document.getElementById("client__search__section"),
+    content: document.getElementById("client__content__section"),
+    insert: document.getElementById("client__insert__section")
+  };
+
+  form() {
+    this.openForm();
+    this.cancelForm();
+    this.submitForm();
+  };
+
+  openForm() {
+    let openBtn = document.getElementById("client__page-title__add__btn");
+  
+    openBtn.onclick = () => {
+      this.hideAllSections();
+      this.sections.insert.classList.remove("--hide");
+    };
 
   };
 
-  insert() {
+  cancelForm() {
+    let cancelBtn = document.getElementById("client__insert__form__submit-box__cancel__btn");
+    cancelBtn.onclick = () => {
+      this.clearFormData();
+      this.showAllSections();
+      this.sections.insert.classList.add("--hide");
+    };
+  };
 
-  }
+  submitForm() {
+    let form = document.getElementById("client__insert__form__form");
+    form.onsubmit = (e) => {
+      e.preventDefault();
+
+      if (this.validation()) {
+        let formData = new FormData(form);
+        formData.append("type", "insert");
+        this.clearFormData();
+
+        (async () => {
+          const res = await fetch('./server/client.php', {
+            method: "POST",
+            header: {
+              ContentType: "application/json"
+            },
+            body: formData
+          });
+
+          const data = await res.json();
+          console.log(data);
+
+        })();
+        
+      }
+    }
+  };
+
+  clearFormData() {
+    document.getElementById("client__insert__form__input-box__input__name").value = "";
+    document.getElementById("client__insert__form__input-box__input__phone").value = "";
+    document.getElementById("client__insert__form__error-box__section").innerHTML = "";
+  };
+
+  validation() {
+    let name = document.getElementById("client__insert__form__input-box__input__name").value;
+    let phone = document.getElementById("client__insert__form__input-box__input__phone").value;
+    let errorsSection = document.getElementById("client__insert__form__error-box__section");
+    let errors = [];
+    errorsSection.innerHTML = "";
+
+    if (!name) {
+      errors.push("Nome inválido");
+    }
+
+    if (!phone) {
+      errors.push("Phone inválido");
+    }
+
+    if (errors.length !== 0) {
+      for (const error of errors) {
+        let span = document.createElement("span");
+        span.classList.add("client__insert__form__error-box__error");
+        span.innerHTML = error;
+        errorsSection.appendChild(span);
+      }
+      return;
+    }
+
+    return true;
+
+  };
+
+  hideAllSections() {
+    for (const section in this.sections) {
+      if (Object.hasOwnProperty.call(this.sections, section)) {
+        this.sections[section].classList.add("--hide");
+      }
+    }
+  };
+
+  showAllSections() {
+    for (const section in this.sections) {
+      if (Object.hasOwnProperty.call(this.sections, section)) {
+        this.sections[section].classList.remove("--hide");
+      }
+    }
+  };
 
   delete() {
 
-  }
+  };
 
   update() {
 
-  }
+  };
 
   selectAll() {
 
-  }
+  };
 
   selectById() {
 
-  }
+  };
 
   search() {
 
-  }
+  };
 
 }
 
--- server/client.php
+++ server/client.php
@@ -3,3 +3,20 @@ $username = "root";
 $password = "";
 
 $conn = new PDO('mysql:host=localhost;dbname=kylogramer', $username, $password);
+
+if (isset($_POST) && !empty($_POST)) {
+  if ($_POST['type'] == "insert") {
+
+    $sql = "INSERT INTO `client`(`name`, `phone`) VALUES (:name, :phone)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':name', $_POST['name']);
+    $stmt->bindParam(':phone', $_POST['phone']);
+    $stmt->execute();
+
+    $sql = "SELECT * FROM client WHERE deleted = 0 ORDER BY id DESC";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
+    exit();
+  }
+}
