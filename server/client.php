769502d906ec3bda956c14befb1553ec141e23da
fernando henrique <fernandomk6@gmail.com>
2022-05-23 17:08:22 -0300

    corrigido identação server client js

--- server/client.php
+++ server/client.php
@@ -32,30 +32,36 @@ if (isset($_POST) && !empty($_POST)) {
   }
 
   if ($_POST['type'] == "delete") {
+
     $sql = "UPDATE `client` SET `deleted` = 1 WHERE id = :id";
     $stmt = $conn->prepare($sql);
     $stmt->bindParam(':id', $_POST['id']);
     $stmt->execute();
     exit();
+
   }
 }
 
 if (isset($_GET) && !empty($_GET)) {
+
   if ($_GET['type'] == "selectAll") {
+
     $sql = "SELECT * FROM client WHERE deleted = 0 ORDER BY id DESC";
     $stmt = $conn->prepare($sql);
     $stmt->execute();
     echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
     exit();
+
   }
 
   if ($_GET['type'] == "selectById") {
+
     $sql = "SELECT * FROM client WHERE deleted = 0 AND id = :id";
     $stmt = $conn->prepare($sql);
     $stmt->bindParam(':id', $_GET['id']);
     $stmt->execute();
-
     echo json_encode($stmt->fetch(PDO::FETCH_ASSOC));
     exit();
+    
   }
 }
